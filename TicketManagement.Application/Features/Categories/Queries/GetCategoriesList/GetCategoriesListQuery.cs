4c78cf02f92ad633ae25ab9c9fd60fcf0a340f73
leandro <leandromtr@hotmail.com>
2021-04-22 00:24:19 +0100

    Add Application layer

---
+++ TicketManagement.Application/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace TicketManagement.Application.Features.Categories.Queries.GetCategoriesList
+{
+    public class GetCategoriesListQuery : IRequest<List<CategoryListVm>>
+    {
+    }
+}
