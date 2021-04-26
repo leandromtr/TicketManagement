3d721ece80e72735181fb5e40e4c5e6357f30402
leandro <leandromtr@hotmail.com>
2021-04-26 09:28:09 +0100

    add identity files

---
+++ TicketManagement.Application/Contracts/Identity/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using TicketManagement.Application.Models.Authentication;
+using System.Threading.Tasks;
+
+namespace TicketManagement.Application.Contracts.Identity
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
+        Task<RegistrationResponse> RegisterAsync(RegistrationRequest request);
+    }
+}
---
+++ TicketManagement.Identity/TicketManagementIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using TicketManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TicketManagement.Identity
+{
+    public class TicketManagementIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public TicketManagementIdentityDbContext(DbContextOptions<TicketManagementIdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
